db83c75c5a75a654bf0da23f13a353e3994db504
Cyril Ajieh <>
2019-02-02 05:55:28 -0500

    E2E test for display Current Location weather information for the current day

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to jst-local-weather-app!');
+    expect(page.getTitleText()).toEqual('JST Local Weather');
   });
 
   afterEach(async () => {
